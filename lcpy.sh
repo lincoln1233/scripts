969349e8d73669ce65af89afb6912b2674847a20
Lincoln <lincoln1233@users.noreply.github.com>
2017-02-12 17:16:48 -0500

    Update lcpy.sh

--- lcpy.sh
+++ lcpy.sh
@@ -1,6 +1,6 @@
 #see readme.... just view file on remote server or wherever using "cat" then "lcpy !!" to send the contents locally.
 #I'm tired of scp'ing files
 #! /bin/sh
-"$@" > ~/lcpy_output.txt
-#lcpy() { eval "$(history -p \!\!)" | xclip -selection c; }
+#"$@" > ~/lcpy_output.txt
+lcpy() { eval "$(history -p \!\!)" | xclip -selection c; }
 
